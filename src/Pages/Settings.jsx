2ddeca973b795f7861e9e267946355e222580b21
marcovidonis <marco@nimblea.pe>
2024-10-17 14:28:51 +0100

    add CheckoutForm component

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -4,6 +4,7 @@ import { Button } from '../components/Button';
 import { ButtonLink } from '../components/ButtonLink';
 import { Layout } from '../layout/Layout';
 import { Typography } from '../components/Typography';
+import { CheckoutForm } from '../components/CheckoutForm';
 
 export function Settings() {
   const { isLoading, signOut, user } = useAuth();
@@ -12,18 +13,21 @@ export function Settings() {
     <Layout>
       <Typography style='h2' className='mb-0'>Account Settings</Typography>
       <Typography style='h4' className='text-gray-500 mt-1 mb-8'>{user?.firstName} {user?.lastName}</Typography>
-      <ButtonLink
-        className='text-ipblue-200 bg-ipblue-800'
-        label='Customer Portal'
-        to='https://billing.stripe.com/p/login/test_eVa2accDp7pj9c4288'
-      />
-      <Button
-        className='text-red-800'
-        onClick={() => signOut()}
-        disabled={isLoading}
-      >
-        Sign Out
-      </Button>
+      <div className='flex flex-col mx-auto max-w-full md:max-w-md'>
+        <CheckoutForm className='mt-6 w-full' />
+        <ButtonLink
+          className='mt-6'
+          label='Manage Subscription'
+          to='https://billing.stripe.com/p/login/test_eVa2accDp7pj9c4288' // FIXME can't be hardcoded
+        />
+        <Button
+          className='text-red-800 mt-6'
+          onClick={() => signOut()}
+          disabled={isLoading}
+        >
+          Sign Out
+        </Button>
+      </div>
     </Layout>
   );
 }
