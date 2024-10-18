8b003d4e4a72242e98a46fff1a11b8612067b0b0
marcovidonis <marco@nimblea.pe>
2024-10-18 12:05:02 +0100

    comment out boilerplate text

--- src/components/PricingCardsGrid.jsx
+++ src/components/PricingCardsGrid.jsx
@@ -20,7 +20,7 @@ export const PricingCardsGrid = ({ products, prices, selectedPriceId, setSelecte
       {/* <!-- Title --> */}
       <div className='max-w-2xl mx-auto text-center mb-10 lg:mb-14'>
         <h2 className='text-2xl font-bold md:text-4xl md:leading-tight dark:text-white'>Pricing</h2>
-        <p className='mt-1 text-gray-600 dark:text-neutral-400'>Whatever your status, our offers evolve according to your needs.</p>
+        {/* <p className='mt-1 text-gray-600 dark:text-neutral-400'>Whatever your status, our offers evolve according to your needs.</p> */}
       </div>
       {/* <!-- End Title --> */}
 
