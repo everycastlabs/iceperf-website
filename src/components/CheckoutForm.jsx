84a65fcf084287d4a1af15d604b01aadb6fbafd4
marcovidonis <marco@nimblea.pe>
2024-10-18 12:04:13 +0100

    show cards to select subscription and price

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types'
+import { twMerge } from 'tailwind-merge';
 
 import { Button } from '../components/Button';
+import { PricingCardsGrid } from './PricingCardsGrid';
 
 export const CheckoutForm = ({ className = '', disabled, userId }) => {
   const [selectedPriceId, setSelectedPriceId] = useState();
@@ -18,7 +20,6 @@ export const CheckoutForm = ({ className = '', disabled, userId }) => {
       setProducts(respJson.productsList);
     }
     getPrices();
-    setSelectedPriceId('price_1Q5VZVChTA44sQl54VmhhNHc');
   }, []);
 
   useEffect(() => {
@@ -33,25 +34,26 @@ export const CheckoutForm = ({ className = '', disabled, userId }) => {
     getStripeCustomer();
   }, [userId]);
 
-  console.log('the prices', prices);
-  console.log('the products', products);
-
   return (
-    <>
-      {/* <Card></Card> */}
+    <div className={twMerge('flex flex-col items-center', className)}>
+      <PricingCardsGrid
+        products={products}
+        prices={prices}
+        selectedPriceId={selectedPriceId}
+        setSelectedPriceId={setSelectedPriceId}
+      />
       <form
-        className={className}
         action={`${import.meta.env.VITE_API_BASE_URI}/api/checkout/create-session/${selectedPriceId}/${customerId}`}
       >
         <Button
           type='submit'
-          className='w-full'
+          className='w-full md:w-52'
           disabled={disabled || !selectedPriceId}
         >
           Go To Checkout
         </Button>
       </form>
-    </>
+    </div>
   )
 };
 
