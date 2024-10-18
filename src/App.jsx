0fd2f7f3252bfa48eef524255043087d01a0a994
marcovidonis <marco@nimblea.pe>
2024-10-18 17:56:47 +0100

    make Pricing page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import { Results } from './Pages/Results';
 import { Provider } from './Pages/Provider';
 import { Login } from './Pages/Login';
 import { Settings } from './Pages/Settings';
+import { Pricing } from './Pages/Pricing';
 
 function App() {
   const location = useLocation();
@@ -28,6 +29,7 @@ function App() {
         <Route path='/about' element={<About />} />
         <Route path='/results' element={<Results />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/pricing' element={<Pricing />} />
         <Route path='/providers/:name' element={<Provider />} />
         <Route path='/projects/:name' element={<Provider isOSSProject />} />
         <Route path='/settings' element={<ProtectedRoute> <Settings /> </ProtectedRoute>} />
--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -4,7 +4,6 @@ import { Button } from '../components/Button';
 import { ButtonLink } from '../components/ButtonLink';
 import { Layout } from '../layout/Layout';
 import { Typography } from '../components/Typography';
-import { CheckoutForm } from '../components/CheckoutForm';
 import { UserContext } from '../contexts/userContext';
 
 export function Settings() {
@@ -15,7 +14,11 @@ export function Settings() {
       <Typography style='h2' className='mb-0'>Account Settings</Typography>
       <Typography style='h4' className='text-gray-500 mt-1 mb-8'>{user?.firstName} {user?.lastName}</Typography>
       <div className='flex flex-col'>
-        <CheckoutForm className='mt-6 w-full' />
+        <ButtonLink
+          label='Start a new subscription'
+          disabled={user?.hasActiveSubscription}
+          to='/pricing'
+        />
         <ButtonLink
           className='mt-6 mx-auto max-w-full md:max-w-md'
           label='Manage Subscription'
